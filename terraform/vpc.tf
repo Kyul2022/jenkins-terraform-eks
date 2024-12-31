46b1fd90d376747587697a951eb375280bf00820
Kyul <106420255+Kyul2022@users.noreply.github.com>
2024-12-31 08:57:22 +0100

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,96 +1,28 @@
 data "aws_availability_zones" "azs" {}
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-resource "aws_vpc" "myapp_vpc" {
-  cidr_block           = var.vpc_cidr_block
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+  name = "myapp-vpc"
+  cidr = var.vpc_cidr_block
+
+  azs             =data.aws_availability_zones.azs.names
+  private_subnets = var.private_subnet_cidr_blocks
+  public_subnets  = var.public_subnet_cidr_blocks
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
   tags = {
-    Name = "myapp-vpc"
     "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
   }
-}
 
-resource "aws_subnet" "public_subnets" {
-  count                   = length(var.public_subnet_cidr_blocks)
-  vpc_id                  = aws_vpc.myapp_vpc.id
-  cidr_block              = var.public_subnet_cidr_blocks[count.index]
-  availability_zone       = data.aws_availability_zones.azs.names[count.index]
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "myapp-public-subnet-${count.index}"
+  public_subnet_tags = {
     "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
-    "kubernetes.io/role/elb"                  = "1"
+    "kubernetes.io/role/elb"                  = 1
   }
-}
 
-resource "aws_subnet" "private_subnets" {
-  count             = length(var.private_subnet_cidr_blocks)
-  vpc_id            = aws_vpc.myapp_vpc.id
-  cidr_block        = var.private_subnet_cidr_blocks[count.index]
-  availability_zone = data.aws_availability_zones.azs.names[count.index]
-  tags = {
-    Name = "myapp-private-subnet-${count.index}"
+  private_subnet_tags = {
     "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
-    "kubernetes.io/role/internal-elb"         = "1"
+    "kubernetes.io/role/internal-elb"         = 1
   }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.myapp_vpc.id
-  tags = {
-    Name = "myapp-igw"
-  }
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public_subnets[0].id
-  tags = {
-    Name = "myapp-nat-gateway"
-  }
-}
-
-resource "aws_eip" "nat" {
-  vpc = true
-  tags = {
-    Name = "myapp-nat-eip"
-  }
-}
-
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.myapp_vpc.id
-  tags = {
-    Name = "myapp-public-route-table"
-  }
-}
-
-resource "aws_route" "public_route" {
-  route_table_id         = aws_route_table.public_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
-
-resource "aws_route_table_association" "public_subnet_association" {
-  count          = length(aws_subnet.public_subnets)
-  subnet_id      = aws_subnet.public_subnets[count.index].id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.myapp_vpc.id
-  tags = {
-    Name = "myapp-private-route-table"
-  }
-}
-
-resource "aws_route" "private_route" {
-  route_table_id         = aws_route_table.private_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat.id
-}
-
-resource "aws_route_table_association" "private_subnet_association" {
-  count          = length(aws_subnet.private_subnets)
-  subnet_id      = aws_subnet.private_subnets[count.index].id
-  route_table_id = aws_route_table.private_route_table.id
 }
