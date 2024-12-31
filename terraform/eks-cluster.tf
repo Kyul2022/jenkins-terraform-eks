924b4334ab7a34dea5e83cf38b51dc649df13df1
Kyul <106420255+Kyul2022@users.noreply.github.com>
2024-12-31 14:35:59 +0100

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.31"
 
-  cluster_name    = "example"
+  cluster_name    = "myapp-eks-cluster"
   cluster_version = "1.31"
 
   # Optional
